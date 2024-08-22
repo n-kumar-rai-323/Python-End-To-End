53cc4cdc2354e31577d7ee2cd25a483e66667ead
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-21 19:35:39 -0700

    input wiht practices

---
+++ Chapter_Two/input.py
@@ -0,0 +1,8 @@
+
+
+entnNum  =int (input("Enter your Number One "))
+entnNum2 = int (input("Enter your Number Two "))
+
+Sum = entnNum + entnNum2
+
+print("The Sum is ", Sum)
---
+++ Chapter_Two/practices.py
@@ -0,0 +1,6 @@
+
+# 6. write a python program to calculate the square of a number entered by the user.
+
+a = int(input("Enter a Number"))
+print("The number of square is ", a *a)
+print("The number of square is ", a **2)
