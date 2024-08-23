5fddc01bacc1db2ade2058bd4f88bdd873af7124
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2024-08-22 21:30:20 -0700

    List

---
+++ Chapter-Four/list.py
@@ -0,0 +1,33 @@
+friends = ['Apple', "Orange", 5, 55, False, "Nishan", "Alisha"]
+
+print(friends[0])
+# string are inmutable 
+
+#list are mutable
+
+friends[0]="Banana"
+print(friends)
+
+# list indexing 
+
+
+print(friends[0:3])
+
+# List Method 
+friends.append("Mobile")
+
+print(friends)
+
+
+text =["c","z","c","a","b"]
+
+text.sort()
+print(text)
+
+text.insert(2,44)
+print(text)
+
+
+text.pop(4)
+print(text)
+
